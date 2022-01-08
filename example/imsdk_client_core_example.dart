c3b3c11f74dc44ea9972a40c4ea9e4943ce0dee2
yangfanyu <yangfanyu@aliyun.com>
2022-01-09 07:01:54 +0800

    Initial version.

---
+++ example/imsdk_client_core_example.dart
@@ -0,0 +1,6 @@
+import 'package:imsdk_client_core/imsdk_client_core.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/imsdk_client_core.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library imsdk_client_core;
+
+export 'src/imsdk_client_core_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
